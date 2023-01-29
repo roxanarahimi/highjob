18e6143324c903dac27b17114fbcc4c798ed0300
Roxana <ms.roxanarahimi@gmail.com>
2023-01-29 23:51:24 +0330

    2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,8 +12,8 @@ app:{
         ],
         link: [
             {rel: 'stylesheet', href:'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css', integrity:'sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N', crossorigin: 'anonymous' },
-            {rel: 'stylesheet', href:'/css/fonts.css' },
-            {rel: 'stylesheet', href:'/css/style.css' }
+            {rel: 'stylesheet', href:'css/fonts.css' },
+            {rel: 'stylesheet', href:'css/style.css' }
 
         ],
 
